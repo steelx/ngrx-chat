c3d5dcf56c6fdb41d1e76e601490cec321173c1d
Steel <ajinkya@riva.co>
2017-03-22 16:26:03 +0530

    store data types

---
+++ src/app/store/store-data.ts
@@ -0,0 +1,10 @@
+
+import {Participant} from "../../../shared/model/participant";
+import {Thread} from "../../../shared/model/thread";
+import {Message} from "../../../shared/model/message";
+
+export interface StoreData {
+  participants: {[key: number]: Participant},
+  threads: {[key: number]: Thread},
+  messages: {[key: number]: Message}
+}
