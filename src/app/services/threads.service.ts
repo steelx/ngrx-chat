1d4fa347462a0a0d522c354055582c3df876d1fb
Steel <ajinkya@riva.co>
2017-03-23 12:08:54 +0530

    commonHttpHeaders added with userId args

--- src/app/services/threads.service.ts
+++ src/app/services/threads.service.ts
@@ -3,14 +3,15 @@ import {Http} from "@angular/http";
 import {Observable} from "rxjs/Observable";
 import 'rxjs/add/operator/map'
 import {AllUserData} from "../../../shared/to/all-user-data";
+import {commonHttpHeaders} from "./commonHttpHeaders";
 
 @Injectable()
 export class ThreadsService {
 
   constructor(private _http: Http) { }
 
-  loadAllUserData(): Observable<AllUserData> {
-    return this._http.get('/api/threads')
+  loadAllUserData(userId:number = 1): Observable<AllUserData> {
+    return this._http.get('/api/threads', commonHttpHeaders(userId))
       .map(res => res.json());
   }
 
