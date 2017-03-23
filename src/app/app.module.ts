d5b2f6f80fa7be2dace7702abfd46a98c9bb8786
Steel <ajinkya@riva.co>
2017-03-23 12:32:52 +0530

    simple reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import {StoreModule} from "@ngrx/store";
+import {StoreModule, Action} from "@ngrx/store";
 import { UserSelectionComponent } from './user-selection/user-selection.component';
 import { ThreadSectionComponent } from './thread-section/thread-section.component';
 import { MessageSectionComponent } from './message-section/message-section.component';
 import { ThreadListComponent } from './thread-list/thread-list.component';
 import { MessageListComponent } from './message-list/message-list.component';
 import {ThreadsService} from "./services/threads.service";
-import {INITIAL_APPLICATION_STATE} from "./store/application-state";
+import {INITIAL_APPLICATION_STATE, ApplicationState} from "./store/application-state";
+import {LOAD_USER_THREADS_ACTION} from "./store/actions";
+
+function storeReducer(state: ApplicationState = INITIAL_APPLICATION_STATE, action: Action): ApplicationState {
+
+  switch (action.type) {
+    case LOAD_USER_THREADS_ACTION:
+      return Object.assign({}, state, action.payload);
+    default:
+        return state;
+  }
+}
 
 @NgModule({
   declarations: [
@@ -26,7 +37,7 @@ import {INITIAL_APPLICATION_STATE} from "./store/application-state";
     BrowserModule,
     FormsModule,
     HttpModule,
-    StoreModule.provideStore({}, INITIAL_APPLICATION_STATE)
+    StoreModule.provideStore(storeReducer)
   ],
   providers: [ThreadsService],
   bootstrap: [AppComponent]
